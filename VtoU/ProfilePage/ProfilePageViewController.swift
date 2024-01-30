885581ef5d8e74de560aab66a645638ec2dee970
HuMooole <jjj990407@naver.com>
2024-01-30 20:21:22 +0900

    Setting Change

--- VtoU/ProfilePage/ProfilePageViewController.swift
+++ VtoU/ProfilePage/ProfilePageViewController.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+class ProfilePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Initial
+    }
+    
+}
--- VtoU/TabBarController/TabBarController.swift
+++ VtoU/TabBarController/TabBarController.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 0
+    }
+    
+}
